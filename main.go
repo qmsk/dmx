fe7860cd969ee1462d24a3f4ce557fcd635271b8
Tero Marttila <terom@fixme.fi>
2016-10-17 01:19:53 +0300

    rename github.com/SpComb/qmsk-dmx/artnet; go fmt

--- main.go
+++ main.go
@@ -1,81 +1,80 @@
 package main
 
 import (
-  log "github.com/Sirupsen/logrus"
-  "github.com/qmsk/dmx/artnet"
-  flags "github.com/jessevdk/go-flags"
-  "fmt"
-  "time"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/SpComb/qmsk-dmx/artnet"
+	flags "github.com/jessevdk/go-flags"
+	"time"
 )
 
 var options struct {
-  Options
+	Options
 
-  Artnet  artnet.Config `group:"ArtNet"`
+	Artnet artnet.Config `group:"ArtNet"`
 }
 
 func main() {
-  if args, err := flags.Parse(&options); err != nil {
-    log.Fatalf("flags.Parse")
-  } else if len(args) > 0 {
-    log.Fatalf("Usage")
-  } else {
-    options.Setup()
-  }
+	if args, err := flags.Parse(&options); err != nil {
+		log.Fatalf("flags.Parse")
+	} else if len(args) > 0 {
+		log.Fatalf("Usage")
+	} else {
+		options.Setup()
+	}
 
-  var artnetController *artnet.Controller
-  var discoveryChan = make(chan artnet.Discovery)
+	var artnetController *artnet.Controller
+	var discoveryChan = make(chan artnet.Discovery)
 
-  if c, err := options.Artnet.Controller(); err != nil {
-    log.Fatalf("artnet.Controller: %v", err)
-  } else {
-    log.Infof("artnet.Controller: %v", c)
+	if c, err := options.Artnet.Controller(); err != nil {
+		log.Fatalf("artnet.Controller: %v", err)
+	} else {
+		log.Infof("artnet.Controller: %v", c)
 
-    c.Start(discoveryChan)
+		c.Start(discoveryChan)
 
-    artnetController = c
-  }
+		artnetController = c
+	}
 
+	var artnetAddress = artnet.Address{}
+	var dmxUniverse = artnet.Universe{
+		0x00,
+		0x00,
+		0x00,
+		0x00,
+		0x00,
+	}
 
-  var artnetAddress = artnet.Address{ }
-  var dmxUniverse = artnet.Universe{
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-    0x00,
-  }
+	go func() {
+		for discovery := range discoveryChan {
+			log.Infof("artnet.Discovery:")
 
-  go func() {
-    for discovery := range discoveryChan {
-      log.Infof("artnet.Discovery:")
+			for _, node := range discovery.Nodes {
+				fmt.Printf("%v:\n", node)
 
-      for _, node := range discovery.Nodes {
-        fmt.Printf("%v:\n", node)
+				config := node.Config()
 
-        config := node.Config()
+				fmt.Printf("\tName: %v\n", config.Name)
 
-        fmt.Printf("\tName: %v\n", config.Name)
+				for i, inputPort := range config.InputPorts {
+					fmt.Printf("\tInput %d: %v\n", i, inputPort.Address)
+				}
+				for i, outputPort := range config.OutputPorts {
+					fmt.Printf("\tOutput %d: %v\n", i, outputPort.Address)
 
-        for i, inputPort := range config.InputPorts {
-          fmt.Printf("\tInput %d: %v\n", i, inputPort.Address)
-        }
-        for i, outputPort := range config.OutputPorts {
-          fmt.Printf("\tOutput %d: %v\n", i, outputPort.Address)
+					if err := node.SendDMX(outputPort.Address, dmxUniverse); err != nil {
+						log.Errorf("Node %v: SendDMX: %v", node, err)
+					} else {
+						log.Infof("Node %v: SendDMX", node)
+					}
+				}
+			}
+		}
+	}()
 
-          if err := node.SendDMX(outputPort.Address, dmxUniverse); err != nil {
-            log.Errorf("Node %v: SendDMX: %v", node, err)
-          } else {
-            log.Infof("Node %v: SendDMX", node)
-          }
-        }
-      }
-    }
-  }()
+	for range time.NewTicker(100 * time.Millisecond).C {
+		dmxUniverse[3] += 10
 
-  for range time.NewTicker(100 * time.Millisecond).C {
-    dmxUniverse[3] += 10
-
-    artnetController.SendDMX(artnetAddress, dmxUniverse)
-  }
+		artnetController.SendDMX(artnetAddress, dmxUniverse)
+	}
 }
