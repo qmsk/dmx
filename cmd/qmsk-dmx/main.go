e8e41e2b1705180edba240b23eba23f0cee84960
Tero Marttila <terom@fixme.fi>
2016-10-22 22:31:34 +0300

    qmsk-dmx: animate configured heads on discovered artnet universes

---
+++ cmd/qmsk-dmx/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/SpComb/qmsk-dmx/artnet"
+	"github.com/SpComb/qmsk-dmx/heads"
+	flags "github.com/jessevdk/go-flags"
+	colorful "github.com/lucasb-eyer/go-colorful"
+)
+
+var options struct {
+	Options
+
+	Artnet artnet.Config `group:"ArtNet"`
+	Heads  heads.Options `group:"Heads"`
+
+	Args struct {
+		HeadsConfig string
+	} `positional-args:"yes" required:"yes"`
+}
+
+func main() {
+	if args, err := flags.Parse(&options); err != nil {
+		log.Fatalf("flags.Parse")
+	} else if len(args) > 0 {
+		log.Fatalf("Usage")
+	} else {
+		options.Setup()
+	}
+
+	var artnetController *artnet.Controller
+	var discoveryChan = make(chan artnet.Discovery)
+
+	if c, err := options.Artnet.Controller(); err != nil {
+		log.Fatalf("artnet.Controller: %v", err)
+	} else {
+		log.Infof("artnet.Controller: %v", c)
+
+		c.Start(discoveryChan)
+
+		artnetController = c
+	}
+
+	// heads
+	var headsHeads *heads.Heads
+
+	if headsConfig, err := options.Heads.Config(options.Args.HeadsConfig); err != nil {
+		log.Fatalf("heads.Config %v: %v", options.Args.HeadsConfig, err)
+	} else if heads, err := options.Heads.Heads(headsConfig); err != nil {
+		log.Fatalf("heads.Heads: %v", err)
+	} else {
+		headsHeads = heads
+	}
+
+	// patch heads output universes on artnet discovery
+	go func() {
+		for discovery := range discoveryChan {
+			log.Infof("artnet.Discovery:")
+
+			for _, node := range discovery.Nodes {
+				fmt.Printf("%v:\n", node)
+
+				config := node.Config()
+
+				fmt.Printf("\tName: %v\n", config.Name)
+
+				for i, inputPort := range config.InputPorts {
+					fmt.Printf("\tInput %d: %v\n", i, inputPort.Address)
+				}
+				for i, outputPort := range config.OutputPorts {
+					fmt.Printf("\tOutput %d: %v\n", i, outputPort.Address)
+				}
+			}
+
+			// patch outputs
+			for address, universe := range artnetController.Universes() {
+				// XXX: not safe
+				headsHeads.Output(heads.Universe(address.Integer()), universe)
+			}
+		}
+	}()
+
+	// animate heads
+	var intensity heads.Intensity = 1.0
+
+	for range time.NewTicker(100 * time.Millisecond).C {
+		var color = colorful.FastHappyColor()
+
+		var headsColor = heads.ColorRGB{
+			R: heads.Value(color.R),
+			G: heads.Value(color.G),
+			B: heads.Value(color.B),
+		}
+
+		headsHeads.Each(func(head *heads.Head) {
+			headIntensity := head.Intensity()
+			headColor := head.Color()
+
+			log.Debugf("head %v: intensity=%v color=%v", head, headIntensity.Get(), headColor.Exists())
+
+			if headColor.Exists() {
+				log.Debugf("head %v: Color %v @ %v", head, color, intensity)
+
+				headColor.SetRGBIntensity(headsColor, intensity)
+
+			} else if headIntensity.Exists() {
+				log.Debugf("head %v: Intensity %v", head, intensity)
+
+				headIntensity.Set(intensity)
+			}
+		})
+
+		headsHeads.Refresh()
+
+		intensity *= 0.9
+
+		if intensity < 0.001 {
+			intensity = 1.0
+		}
+	}
+}
