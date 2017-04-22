ffd6a0c9df8eb7b79628027375c5f5fc138c93bd
Tero Marttila <terom@fixme.fi>
2017-04-22 15:59:25 +0300

    web/lib/websocket: convert from subject to pure Observable

--- web/lib/websocket/websocket.ts
+++ web/lib/websocket/websocket.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
-import { Subject } from 'rxjs/Subject';
 
 export enum ReadyState {
     Connecting  = 0,
@@ -18,36 +17,47 @@ export class WebSocketError extends Error {
 
 @Injectable()
 export class WebSocketService {
-  public connect<T>(url: string): Subject<T> {
+  public connect<T>(url: string): Observable<T> {
     if (url[0] == '/') {
       url = (location.protocol == 'http:' ? 'ws:' : 'wss:') + '//' + location.host + url;
     }
-    let ws = new WebSocket(url);
-    let subject = new Subject<T>();
 
-    ws.onopen = (event: Event) => {
-      // wait for first message before advancing subject
-    };
-    ws.onerror = (event: Event) => {
-      // the websocket error event is intentionally useless, it does not contain any other information
-      // just wait for the close event...
-      console.log("WebSocket onerror", event);
-    };
-    ws.onmessage = (event: MessageEvent) => {
-      subject.next(JSON.parse(event.data));
-    };
-    ws.onclose = (closeEvent: CloseEvent) => {
-      let error = new WebSocketError("Websocket closed with code=" + closeEvent.code + ": " + closeEvent.reason);
-      error.code = closeEvent.code;
-      error.reason = closeEvent.reason;
+    return Observable.create(function (observer: Observer<T>) {
+      console.log("WebSocket connect");
 
-      if (closeEvent.wasClean) {
-        subject.complete();
-      } else {
-        subject.error(error);
+      let ws = new WebSocket(url);
+
+      ws.onopen = (event: Event) => {
+        // wait for first message before advancing subject
+        console.log("WebSocket open");
+      };
+      ws.onerror = (event: Event) => {
+        // the websocket error event is intentionally useless, it does not contain any other information
+        // just wait for the close event...
+        console.log("WebSocket error", event);
+      };
+      ws.onmessage = (event: MessageEvent) => {
+        observer.next(JSON.parse(event.data));
+      };
+      ws.onclose = (closeEvent: CloseEvent) => {
+        console.log("WebSocket close", closeEvent);
+
+        let error = new WebSocketError("Websocket closed with code=" + closeEvent.code + ": " + closeEvent.reason);
+        error.code = closeEvent.code;
+        error.reason = closeEvent.reason;
+
+        if (closeEvent.wasClean) {
+          observer.complete();
+        } else {
+          observer.error(error);
+        }
+      };
+
+      return function() {
+        console.log("WebSocket disconnect");
+
+        ws.close();
       }
-    };
-
-    return subject;
+    });
   }
 }
